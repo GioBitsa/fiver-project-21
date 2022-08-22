d9fbee628f5b7775a625711dd00e82f830a1cac1
giobitsa <giorgi.bitsadze01@gmail.com>
2022-08-22 14:46:04 +0400

    added react slick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Teritories, Home } from "./Pages";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
